a1d9601bc7874ee515c200d2c632b3d9dc6cd772
Tim Weissman <tweissm35@charlottesvilleschools.org>
2020-10-20 02:11:05 +0100

    updated dice

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -6,8 +6,8 @@ from random import randint
 print ("Automatic Dice Roller:") 
 print("Press enter to roll or 'x' and then enter to exit.")
 while True:
-  user=input()#waits for the user to type and then press enter
-  if len(user)==0:#If what the user typed nothing and then pressed enter it would be length zero
+  action=input()#waits for the user to type and then press enter
+  if len(action)==0:#Checks if the thing entered is length zero
     print(randint(1,6))#pick random number 1-6
-  elif user == "x":
+  elif action == "x":
     break#stop
