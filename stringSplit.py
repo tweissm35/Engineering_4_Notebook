45364780740cfd674d472d14323ee5cf049c18a4
Tim Weissman <tweissm35@charlottesvilleschools.org>
2020-11-16 03:08:35 +0000

    added string split

--- stringSplit.py
+++ stringSplit.py
@@ -5,4 +5,4 @@ arr = s.split(" ")#make a list with each element being a word separated by a spa
 for i in range(len(arr)):#repeat for each word
 	for j in range(len(arr[i])):#repeat for each letter
 		print(arr[i][j])#prints a letter
-	print("-")
+	print("-")#once the above for loop  finishes the dash can be placed
