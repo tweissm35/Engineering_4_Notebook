538b3559324aeff5fc73a35bcfb267738b307327
Tim Weissman <tweissm35@charlottesvilleschools.org>
2020-11-16 18:18:40 +0000

    Added string split after repo got corrupted

---
+++ Python/stringSplit.py
@@ -0,0 +1,8 @@
+s=input("Please enter a sentence: ")
+
+arr = s.split(" ")#make a list with each element being a word separated by a sp$
+
+for i in range(len(arr)):#repeat for each word
+        for j in range(len(arr[i])):#repeat for each letter
+                print(arr[i][j])#prints a letter
+        print("-")#once the above for loop  finishes the dash can be placed
