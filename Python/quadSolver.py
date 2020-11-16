2756e8cc4d3fb8fb9cd9741c1ad2fc7a1f662dc1
Tim Weissman <tweissm35@charlottesvilleschools.org>
2020-11-16 21:14:03 +0000

    Test commit for quadSolver

---
+++ Python/quadSolver.py
@@ -0,0 +1,9 @@
+import math
+
+print("Quadratic Solver")
+print("Enter the coefficients for ax^2 + bx + c = 0")
+a = input("Enter a")
+b = input("Enter b")
+c = input("Enter c")
+
+print(str(b^2-4ac))
