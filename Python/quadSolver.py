44ad20a8c8bbc9adb03cf217a4b73ec728daf9f8
Tim Weissman <tweissm35@charlottesvilleschools.org>
2020-11-17 04:49:57 +0000

    Fixed a dumb mistake

--- Python/quadSolver.py
+++ Python/quadSolver.py
@@ -9,11 +9,11 @@ def quadSolve(a,b,c):#function called quadSolve that takes three inputs
         elif disc == 0:#if zero
                 return [-b/(2*a)]#return formula w/o the discriminant
         else:#otherwise
-                return [(-b-math.sqrt(b**2-4*a*c))/(2*a),(-b+math.sqrt(b**2-4*a*c))/(2*a)]#full quadratic formula
+                return [(-b-math.sqrt(disc))/(2*a),(-b+math.sqrt(disc))/(2*a)]#full quadratic formula
 
 print("Quadratic Solver")
 print("Enter the coefficients for ax^2 + bx + c = 0")
-a =int(input("Enter a: "))#gets input for a
+a =int(input("Enter a: "))#gets input for a, then casts it as an integer
 b = int(input("Enter b: "))
 c = int(input("Enter c: "))
 print(quadSolve(a,b,c))#print result
