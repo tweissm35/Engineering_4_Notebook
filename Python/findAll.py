0143088c73a2c7033ea86a2ce690bfad950d9208
Tim <tweissm35@charlottesvilleschools.org>
2020-11-17 20:34:42 -0500

    Complemented GeeksforGeeks

--- Python/findAll.py
+++ Python/findAll.py
@@ -1,2 +1,4 @@
 def findAll(string, letter):
   return [i for i in range(len(string)) if string.startswith(letter, i)]
+  #This elegent bit of code is from https://www.geeksforgeeks.org/python-all-occurrences-of-substring-in-string
+  #It uses list comprehension with the startswith() method to find substrings all on one line!
