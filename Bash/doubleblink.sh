0c48ec0e5a3820c176f1e785b702915023e46812
Tim <tweissm35@charlottesvilleschools.org>
2020-12-01 01:11:06 -0500

    added comments and made better

--- Bash/doubleblink.sh
+++ Bash/doubleblink.sh
@@ -1,14 +1,12 @@
-gpio mode 0 out
-gpio mode 1 out
+gpio mode 0 out#set the pins to output mode
+gpio mode 1 out#I only need to do this once
 
-for n in {1..10}
+for n in {1..10}#for loop that repeats 10 times
 do
-	gpio write 0 0
+	gpio write 0 1#on
+	gpio write 1 1
+	sleep 0.5#wait half a second
+	gpio write 0 0#off
 	gpio write 1 0
 	sleep 0.5
-	gpio write 0 1
-	gpio write 1 1
-	sleep 0.5
-gpio write 0 0
-gpio write 1 0
-done
+done#end for loop
