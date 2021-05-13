d5ccbc611e7170c11adf695293f3e65876839ff4
Tim <tweissm35@charlottesvilleschools.org>
2021-05-13 14:54:26 +0100

    did flask

--- Python/headless.py
+++ Python/headless.py
@@ -42,8 +42,8 @@ draw = ImageDraw.Draw(image)
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 # Load default font.
-#font = ImageFont.load_default()
-font = ImageFont.truetype(font="/home/pi/Documents/Engineering_4_Notebook/Pixeled.ttf")
+font = ImageFont.load_default()
+#font = ImageFont.truetype(font="/home/pi/Documents/Engineering_4_Notebook/slkscr.ttf")
 
 while  True:
 	#clear display
@@ -65,7 +65,8 @@ while  True:
 	# Write the accelerometer values
 	#draw.text((0, 0), 'x accel: '+str(accel_x),  font=font, fill=255)
 
-	draw.text((0,0), 'edges represent 12ms^2',font=font, fill=255)
+	draw.text((0,0), 'edges=',font=font, fill=255)
+	draw.text((0,10), '12ms^2',font=font, fill=255)
 
 	draw.line([(64,0),(64,64)],width=1, fill=255)
 	draw.line([(32,32),(96,32)],width=1, fill=255)
